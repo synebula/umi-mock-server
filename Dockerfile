da8b53472617618e046f4c0d077729a8467a5c1f
alex <reizero@live.com>
2022-10-27 10:12:47 +0800

    修改打包方式

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,10 @@ MAINTAINER alex <reizero@live.com>
 #工作目录
 WORKDIR /app
 
-COPY package.json ./
-COPY node_modules ./node_modules
+COPY package-pro.json ./package.json
 COPY lib ./lib
+
+RUN npm config set registry https://registry.npm.taobao.org && npm install
 
 #暴露3000端口
 EXPOSE 3000
--- mock/analyze.ts
+++ mock/analyze.ts
@@ -3,17 +3,20 @@ import { devices, finance, messages, payments, persons, shoppings, trackers as t
 export default {
   'GET /api/search': (req: any, res: any) => {
     let data = [];
-    if (req.query.key === '18899659965') data.push(persons[0]);
-    else data = persons.map((p) => ({ ...p, name: p.name + req.query.key }));
+    data.push(persons[0]);
     if (req)
       res.json({
         success: true,
-        data: {
-          page: 1,
-          size: 10,
-          total: data.length === 1 ? 1 : 100,
-          data,
-        },
+        data: persons[0],
+        status: 200,
+      });
+  },
+  'GET /api/search/history': (req: any, res: any) => {
+    let data = ['18899659965', '358882046176540', 'db37a67f90126073ad807ca778e2ad66'];
+    if (req)
+      res.json({
+        success: true,
+        data,
         status: 200,
       });
   },
@@ -50,13 +53,45 @@ export default {
       });
   },
 
-  'GET /api/analyze/:id/messages': (req: any, res: any) => {
+  'GET /api/analyze/:id/message/types': (req: any, res: any) => {
+    const types = [
+      { id: 1, name: '出行信息', count: 0 },
+      { id: 2, name: '酒店住宿', count: 0 },
+      { id: 3, name: '收寄件', count: 0 },
+      { id: 4, name: '休闲娱乐', count: 0 },
+      { id: 5, name: '金融借贷', count: 0 },
+      { id: 6, name: '境外金盘', count: 0 },
+    ];
     const id = req.params.id;
-    const data = messages.filter((m) => m.owner.toString() === id);
+    const m = messages.filter((m) => m.owner.toString() === id);
+    let data = types.map((t) => ({
+      ...t,
+      count: m.filter((m) => m.type === t.id).length,
+    }));
+
+    data = [{ id: 0, name: '全部短信内容', count: m.length }].concat(data);
     if (req)
       res.json({
         success: true,
-        data: { page: 1, size: 5, total: 100, data },
+        data,
+        status: 200,
+      });
+  },
+
+  'GET /api/analyze/:id/messages': (req: any, res: any) => {
+    const id = req.params.id;
+    const type = req.query.type;
+    const data = messages.filter((m) => {
+      let pass = m.owner.toString() === id;
+      if (pass && type && type !== '0') {
+        pass = m.type?.toString() === type;
+      }
+      return pass;
+    });
+    if (req)
+      res.json({
+        success: true,
+        data: { page: 1, size: 5, total: data.length, data },
         status: 200,
       });
   },
@@ -67,7 +102,7 @@ export default {
     if (req)
       res.json({
         success: true,
-        data: { page: 1, size: 5, total: 100, data },
+        data: { page: 1, size: 5, total: data.length, data },
         status: 200,
       });
   },
@@ -97,7 +132,7 @@ export default {
     if (req)
       res.json({
         success: true,
-        data: { page: 1, size: 10, total: 100, data },
+        data: { page: 1, size: 10, total: data.length, data },
         status: 200,
       });
   },
@@ -116,7 +151,7 @@ export default {
     if (req)
       res.json({
         success: true,
-        data: { page: 1, size: 10, total: 100, data },
+        data: { page: 1, size: 10, total: data.length, data },
         status: 200,
       });
   },
